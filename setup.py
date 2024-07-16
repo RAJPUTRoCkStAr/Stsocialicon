5fc8677b8a2ff30cbf4f1eea281087d22a65c3e4
unknown <sumitsingh9441@gmail.com>
2024-07-16 12:11:29 +0530

    completed

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Stsocialicon',
+    version='0.0.2',
     packages=find_packages(),
     author='Sumit Kumar Singh',
     author_email='sumitsingh9441@gmail.com',
